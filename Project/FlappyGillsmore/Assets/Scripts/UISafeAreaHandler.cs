7344c04b072d89f6b2a1670eb65a0bb2cc71b6fb
Rowan Noble <rowan.noble@uwclub.net>
2023-11-16 21:08:51 +0000

    General UI Adjustments, Fixed Layering
    UI has been adjusted further to have more accurate results between screen sizes. UI layering has also been adjusted in the Game scene so that the background and main UI elements are on separate canvases, with their layer ordering being -1 and 1 respectively, with prefabs layer being 0. This means UI elements render on top of prefabs, fixing issues on the game over screen where sharks would appear over the UI.
    
    A script has been implemented from lab tutorials to adjust UI according to the safe area of a device (avoiding notches for example). Currently the script is not being used.

---
+++ Project/FlappyGillsmore/Assets/Scripts/UISafeAreaHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISafeAreaHandler : MonoBehaviour
+{
+    RectTransform panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        Rect area = Screen.safeArea;
+        // Pixel size in screen space of the whole screen
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+        // Set anchors to percentages of the screen used
+        panel.anchorMin = area.position / screenSize;
+        panel.anchorMax = (area.position + area.size) / screenSize;
+    }
+}
